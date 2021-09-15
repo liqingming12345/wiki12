d7afe257e7d6ca68639ac3a4af4906602ce8c321
apple-lab <apple>
2021-09-16 01:22:50 +0800

    3-6增加通用的返回类CommomResp,以后所有的后端接口返回值全部是CommomResp类型

--- src/main/java/com/jiawa/wiki/controller/DemoController.java
+++ src/main/java/com/jiawa/wiki/controller/DemoController.java
@@ -2,23 +2,22 @@ package com.jiawa.wiki.controller;
 
 import com.jiawa.wiki.domain.Demo;
 import com.jiawa.wiki.service.DemoService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@RequestMapping("/demo")
 public class DemoController {
 
     @Resource
     private DemoService demoService;
 
-    @GetMapping("/demo/list")
-    public List<Demo> list(){
-
+    @GetMapping("/list")
+    public List<Demo> list() {
         return demoService.list();
     }
 }
