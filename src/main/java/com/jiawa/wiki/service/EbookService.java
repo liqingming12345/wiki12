40d794cb80b1132b0a49a2c521960d4b30babf18
apple-lab <apple>
2021-09-16 09:57:46 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -6,6 +6,7 @@ import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
+import com.jiawa.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -26,12 +27,19 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
+       /* List<EbookResp> respList = new ArrayList<>();
         for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
+            //EbookResp ebookResp = new EbookResp();
+            //BeanUtils.copyProperties(ebook,ebookResp);
+
+            //对象复制
+            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+
             respList.add(ebookResp);
-        }
-        return respList;
+        }*/
+
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        return list;
     }
 }
