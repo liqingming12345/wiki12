acfa70a79618c630cbb4640bec2c4f76173600bf
apple-lab <apple>
2021-09-18 09:58:32 +0800

    5-2 使用axios调用后端接口/ebook/list

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -3,8 +3,6 @@
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
           mode="inline"
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
           :style="{ height: '100%', borderRight: 0 }"
       >
         <a-sub-menu key="sub1">
@@ -52,7 +50,7 @@
         <a-breadcrumb-item>App</a-breadcrumb-item>
       </a-breadcrumb>
       <a-layout-content
-          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+          :style="{ background: '#FF00FF', padding: '24px', margin: 0, minHeight: '280px' }"
       >
         Content
       </a-layout-content>
@@ -62,9 +60,15 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
-
+  setup(){
+    console.log("setup");
+    axios.get("http://localhost:8880/ebook/list?name=Spring").then((response)=>{
+      console.log(response);
+    })
+  }
 });
 </script>
