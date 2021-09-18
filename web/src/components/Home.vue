e41e23399465878ff437059585fff58b07ec01eb
apple-lab <apple>
2021-09-18 11:41:27 +0800

    5-3 使用ref和reactive实现数据绑定

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -43,32 +43,43 @@
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
-    <a-layout style="padding: 0 24px 24px">
-      <a-breadcrumb style="margin: 16px 0">
-        <a-breadcrumb-item>Home</a-breadcrumb-item>
-        <a-breadcrumb-item>List</a-breadcrumb-item>
-        <a-breadcrumb-item>App</a-breadcrumb-item>
-      </a-breadcrumb>
       <a-layout-content
-          :style="{ background: '#FF00FF', padding: '24px', margin: 0, minHeight: '280px' }"
+          :style="{ background: '#48D1CC', padding: '24px', margin: 0, minHeight: '280px' }"
       >
-        Content
+        <pre>
+  {{ ebooks }}
+  {{ ebooks2 }}
+        </pre>
+
       </a-layout-content>
     </a-layout>
-  </a-layout>
+
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent , onMounted,  ref, reactive,toRef} from 'vue';
 import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
   setup(){
     console.log("setup");
-    axios.get("http://localhost:8880/ebook/list?name=Spring").then((response)=>{
-      console.log(response);
-    })
+    const ebooks = ref();
+    const ebooks1 = reactive({books:[]});
+    onMounted(() => {
+      console.log("onMounted");
+      axios.get("http://localhost:8880/ebook/list?name=Spring").then((response)=>{
+        const data = response.data;
+        ebooks.value = data.content;
+        ebooks1.books = data.content;
+        console.log(response);
+      });
+    });
+
+    return{
+      ebooks,
+      ebooks2:toRef(ebooks1,"books")
+    }
   }
 });
 </script>
