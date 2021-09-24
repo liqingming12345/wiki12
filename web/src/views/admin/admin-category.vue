6838f050c7f6dc1dc0668b3e8cbb6667c9ac224c
apple-lab <apple>
2021-09-24 16:43:00 +0800

    7-4  分类管理不显示分页

---
+++ web/src/views/admin/admin-category.vue
@@ -0,0 +1,206 @@
+<template>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <p>
+        <a-form layout="inline" :model="param">
+          <a-form-item>
+             <a-button type="primary" @click="handleQuery({})">
+                        查询
+              </a-button>
+              </a-form-item>
+               <a-form-item>
+               <a-button type="primary" @click="add()">
+                        新增
+                </a-button>
+                </a-form-item>
+            </a-form>
+      </p>
+      <a-table
+          :columns="columns"
+          :row-key="record => record.id"
+          :data-source="categorys"
+          :loading="loading"
+          :pagination="false"
+      >
+        <template #cover="{ text: cover }">
+          <img v-if="cover" :src="cover" alt="avatar" />
+        </template>
+        <template v-slot:action="{text, record }">
+          <a-space size="small">
+            <a-button type="primary" @click="edit(record)">
+              编辑
+            </a-button>
+            <a-popconfirm
+                title="删除后不可恢复，确认删除?"
+                ok-text="是"
+                cancel-text="否"
+                @confirm="handleDelete(record.id)"
+            >
+              <a-button type="danger">
+                删除
+              </a-button>
+            </a-popconfirm>
+          </a-space>
+        </template>
+      </a-table>
+    </a-layout-content>
+  </a-layout>
+
+  <a-modal
+      title="分类表单"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @ok="handleModalOk"
+  >
+    <a-form :model="category" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+
+            <a-form-item label="名称">
+              <a-input v-model:value="category.name" />
+            </a-form-item>
+            <a-form-item label="父分类">
+              <a-input v-model:value="category.parent" />
+            </a-form-item>
+            <a-form-item label="顺序">
+              <a-input v-model:value="category.sort" />
+            </a-form-item>
+          </a-form>
+  </a-modal>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import axios from 'axios';
+import{ message } from 'ant-design-vue';
+import {Tool} from "@/util/tool";
+
+
+export default defineComponent({
+  name: 'AdminCategory',
+  setup() {
+    const param = ref();
+    param.value = {};
+    const categorys = ref();
+
+    const loading = ref(false);
+
+    const columns = [
+
+      {
+        title: '名称',
+        dataIndex: 'name'
+      },
+      {
+        title: '父分类',
+        key: 'parent',
+        dataIndex: 'parent'
+      },
+      {
+        title: '顺序',
+        dataIndex: 'sort'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        slots: { customRender: 'action' }
+      },
+      //console.log(response)
+    ];
+
+    /**
+     * 数据查询
+     **/
+    const handleQuery = () => {
+      loading.value = true;
+      //console.log(categorys);
+      axios.get(process.env.VUE_APP_SERVER + "/category/all", ).then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if(data.success){
+          categorys.value = data.content;
+        }else{
+          message.error(data.message);
+        }
+      });
+    };
+
+    // -------- 表单 ---------
+    const category = ref({});
+    const modalVisible = ref(false);
+    const modalLoading = ref(false);
+    const handleModalOk = () => {
+      modalLoading.value = true;
+      axios.post(process.env.VUE_APP_SERVER + "/category/save", category.value).then((response) => {
+        modalLoading.value=false;
+        const data = response.data;  //data = commonResp
+        if(data.success){
+          modalVisible.value = false;
+          //重新加载列表
+          handleQuery();
+        } else{
+          message.error(data.message);
+        }
+       });
+    };
+
+    /**
+     * 编辑
+     */
+    const edit = (record: any) => {
+      modalVisible.value = true;
+      category.value = Tool.copy(record);
+    };
+
+    /**
+     * 新增
+     */
+    const add = () => {
+      modalVisible.value = true;
+      category.value = {};
+    };
+    /**
+     *
+     * 删除
+     */
+    const handleDelete = (id:number) =>{
+      axios.delete(process.env.VUE_APP_SERVER + "/category/delete/"+ id).then((response) => {
+        const data = response.data;  //data = commonResp
+        if(data.success){
+          //重新加载列表
+          handleQuery();
+        }
+      });
+    };
+
+    onMounted(() => {
+      handleQuery();
+    });
+
+    return {
+      param,
+      categorys,
+      columns,
+      loading,
+      handleQuery,
+
+      edit,
+      add,
+
+      category,
+      modalVisible,
+      modalLoading,
+      handleModalOk,
+
+      handleDelete
+    }
+  }
+});
+</script>
+
+<style scoped>
+img {
+  width: 50px;
+  height: 50px;
+}
+</style>
