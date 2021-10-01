df688806b299e25a2bdc4eb9d4f1ae1b651d95f2
apple-lab <apple>
2021-10-02 01:57:18 +0800

    9-9 使用vuex保存用户信息，方便其他组件获取登录信息。遗留问题，刷新后值没有了。

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -4,15 +4,14 @@
     <a-menu
         theme="dark"
         mode="horizontal"
-
         :style="{ lineHeight: '64px' }"
     >
       <a-menu-item key="/">
         <router-link to="/">首页</router-link>
       </a-menu-item>
       <a-menu-item key="/admin/user">
-                <router-link to="/admin/user">用户管理</router-link>
-              </a-menu-item>
+        <router-link to="/admin/user">用户管理</router-link>
+      </a-menu-item>
       <a-menu-item key="/admin/ebook">
         <router-link to="/admin/ebook">电子书管理</router-link>
       </a-menu-item>
@@ -22,82 +21,93 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" @click="showLoginModal">
-                <span>登录</span>
+      <a class="login-menu" v-show="user.id">
+        <span>您好：{{user.name}}</span>
+      </a>
+      <a class="login-menu" v-show="!user.id" @click="showLoginModal">
+        <span>登录</span>
       </a>
     </a-menu>
 
     <a-modal
-              title="登录"
-              v-model:visible="loginModalVisible"
-              :confirm-loading="loginModalLoading"
-              @ok="login"
-            >
-            <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-              <a-form-item label="登录名">
-                <a-input v-model:value="loginUser.loginName" />
-              </a-form-item>
-              <a-form-item label="密码">
-                <a-input v-model:value="loginUser.password" type="password" />
-              </a-form-item>
-            </a-form>
-          </a-modal>
+        title="登录"
+        v-model:visible="loginModalVisible"
+        :confirm-loading="loginModalLoading"
+        @ok="login"
+    >
+      <a-form :model="loginUser" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+        <a-form-item label="登录名">
+          <a-input v-model:value="loginUser.loginName" />
+        </a-form-item>
+        <a-form-item label="密码">
+          <a-input v-model:value="loginUser.password" type="password" />
+        </a-form-item>
+      </a-form>
+    </a-modal>
   </a-layout-header>
 </template>
 
 <script lang="ts">
-
 import { defineComponent, ref } from 'vue';
 import axios from 'axios';
-  import { message } from 'ant-design-vue';
+import { message } from 'ant-design-vue';
+import store from "@/store";
 
-      declare let hexMd5: any;
-  declare let KEY: any;
+declare let hexMd5: any;
+declare let KEY: any;
 
 export default defineComponent({
-      name: 'the-header',
-        setup () {
-        const loginUser = ref({
-              loginName: "test",
-              password: "test"
-        });
-        const loginModalVisible = ref(false);
-        const loginModalLoading = ref(false);
-        const showLoginModal = () => {
-            loginModalVisible.value = true;
-          };
+  name: 'the-header',
+  setup () {
+    // 登录后保存
+    const user = ref();
+    user.value = {};
 
-            // 登录
-                const login = () => {
-                  console.log("开始登录");
-                          loginModalLoading.value = true;
-                          loginUser.value.password = hexMd5(loginUser.value.password + KEY);
-                          axios.post(process.env.VUE_APP_SERVER + '/user/login', loginUser.value).then((response) => {
-                              loginModalLoading.value = false;
-                             const data = response.data;
-                              if (data.success) {
-                                  loginModalVisible.value = false;
-                                  message.success("登录成功！");
-                                } else {
-                                  message.error(data.message);
-                                }
-                            });
-          };
+    // 用来登录
+    const loginUser = ref({
+      loginName: "test",
+      password: "test123"
+    });
+    const loginModalVisible = ref(false);
+    const loginModalLoading = ref(false);
+    const showLoginModal = () => {
+      loginModalVisible.value = true;
+    };
 
-            return {
-          loginModalVisible,
-              loginModalLoading,
-              showLoginModal,
-              loginUser,
-          login
+    // 登录
+    const login = () => {
+      console.log("开始登录");
+      loginModalLoading.value = true;
+      loginUser.value.password = hexMd5(loginUser.value.password + KEY);
+      axios.post(process.env.VUE_APP_SERVER + '/user/login', loginUser.value).then((response) => {
+        loginModalLoading.value = false;
+        const data = response.data;
+        if (data.success) {
+          loginModalVisible.value = false;
+          message.success("登录成功！");
+          user.value = data.content;
+          store.commit("setUser",user.value);
+        } else {
+          message.error(data.message);
         }
-      }
+      });
+    };
+
+    return {
+      loginModalVisible,
+      loginModalLoading,
+      showLoginModal,
+      loginUser,
+      login,
+      user
+    }
+  }
 });
 </script>
 
 <style>
-  .login-menu {
-    float: right;
-    color: white;
-  }
+.login-menu {
+  float: right;
+  color: white;
+}
 </style>
